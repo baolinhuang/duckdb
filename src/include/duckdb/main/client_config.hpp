ef7a6fa8b7139a97e89892bd5c64770e69593c63
tianjingqi.tjq <tianjingqi.tjq@alibaba-inc.com>
2025-11-27 10:42:22 +0800

    Set order_by_non_integer_literal = true

--- src/include/duckdb/main/client_config.hpp
+++ src/include/duckdb/main/client_config.hpp
@@ -132,7 +132,7 @@ struct ClientConfig {
 	//! Use IEE754-compliant floating point operations (returning NAN instead of errors/NULL)
 	bool ieee_floating_point_ops = false;
 	//! Allow ordering by non-integer literals - ordering by such literals has no effect
-	bool order_by_non_integer_literal = false;
+	bool order_by_non_integer_literal = true;
 	//! Disable casting from timestamp => timestamptz (naïve timestamps)
 	bool disable_timestamptz_casts = false;
 	//! If DEFAULT or ENABLE_SINGLE_ARROW, it is possible to use the deprecated single arrow operator (->) for lambda
